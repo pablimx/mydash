eae5162d75043ad7e7c4cd1d7de1f3789391c46c
Pablo Resende <pablo.resende@luizalabs.com>
2021-10-11 18:28:36 -0300

    feat: creating charts and Users page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,66 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, SimpleGrid, Box, Text, theme } from '@chakra-ui/react'
+import dynamic from 'next/dynamic'
 import { Header } from "../components/Header"
 import { Sidebar } from '../components/Sidebar'
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false
+})
+
+const options = {
+  chart: {
+    toolbar: {
+      show: false
+    },
+    zoom: {
+      enabled: false
+    },
+    foreColor: theme.colors.gray[500]
+  },
+  grid: {
+    show: false
+  },
+  dataLabels: {
+    enabled: false
+  },
+  tooltip: {
+    enabled: false
+  },
+  xaxis: {
+    type: 'datetime' as const,
+    axisBorder: {
+      color: theme.colors.gray[600]
+    },
+    axisTicks: {
+      color: theme.colors.gray[600]
+    },
+    categories: [
+      '2021-10-04T00:00:00.000Z',
+      '2021-10-05T00:00:00.000Z',
+      '2021-10-06T00:00:00.000Z',
+      '2021-10-07T00:00:00.000Z',
+      '2021-10-08T00:00:00.000Z',
+      '2021-10-09T00:00:00.000Z',
+      '2021-10-10T00:00:00.000Z',
+    ]
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3
+    }
+  }
+}
+
+const series = [
+  {
+    name: 'series1',
+    data: [31, 120, 10, 28, 61, 18, 109]
+  }
+]
 
 export default function Dashboard() {
   return(
@@ -14,6 +74,25 @@ export default function Dashboard() {
         px="6"
       >
         <Sidebar />
+        <SimpleGrid
+          flex="1"
+          gap="4"
+          minChildWidth="320px"
+          align="flex-start"
+        >
+          <Box p="8" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Subscribers of the week
+            </Text>
+            <Chart type="area" height={160} options={options} series={series} />
+          </Box>
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">
+              Open rate
+            </Text>
+            <Chart type="area" height={160} options={options} series={series} />
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   )
---
+++ src/pages/users/index.tsx
@@ -0,0 +1,96 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Button,
+  Icon,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Checkbox,
+  Tbody,
+  Td,
+  Text
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+      <Flex
+        w="100%"
+        my="6"
+        maxWidth={1480}
+        mx="auto"
+        px="6"
+      >
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Users
+            </Heading>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Create new
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>
+                  User
+                </Th>
+                <Th>
+                  Included at
+                </Th>
+                <Th width={8} />
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td px="6">
+                  <Box>
+                    <Text fontWeight="bold">John Doe</Text>
+                    <Text fontSize="sm" color="gray.300">john.doe@test.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  11 de Outubro de 2021
+                </Td>
+                <Td>
+                <Button
+                  as="a"
+                  size="sm"
+                  fontSize="sm"
+                  colorScheme="purple"
+                  leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                >
+                  Edit
+                </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
