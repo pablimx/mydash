cd5122802c7cfc09792b184a52ee2f1f15a9a9b3
Pablo Resende <pablo.resende@luizalabs.com>
2021-10-14 20:44:20 -0300

    feat: configuring miragejs as a server for the application

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react"
+import Link from 'next/link'
 import {
   Box,
   Flex,
@@ -15,17 +17,22 @@ import {
   useBreakpointValue,
   IconButton
 } from "@chakra-ui/react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { Header } from "../../components/Header";
-import { Pagination } from "../../components/Pagination";
-import { Sidebar } from "../../components/Sidebar";
-import Link from 'next/link'
+import { RiAddLine, RiPencilLine } from "react-icons/ri"
+import { Header } from "../../components/Header"
+import { Pagination } from "../../components/Pagination"
+import { Sidebar } from "../../components/Sidebar"
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
   })
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
 
   return (
     <Box>
